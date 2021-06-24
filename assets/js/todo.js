2792dc783703cd3b91740ca34de3f10f19c9387b
David Ogunsola <78702123+Davidson019@users.noreply.github.com>
2021-06-24 19:34:48 +0100

    Another commit

---
+++ assets/js/todo.js
@@ -0,0 +1,26 @@
+let app = angular.module('todoApp', []);
+
+app.controller('todoCtrl', ['$scope', ($scope)=>{
+    $scope.todoList = [];
+
+    $scope.addTodo = ()=>{
+        $scope.todoList.push({
+            title: $scope.newTodo.title,
+            description: $scope.newTodo.description,
+            date: {
+                day: $scope.newTodo.date.getDate(),
+                month: $scope.newTodo.date.getMonth(),
+                year: $scope.newTodo.date.getFullYear()
+            }
+        });
+
+        $scope.newTodo.title = '';
+        $scope.newTodo.description = '';
+        $scope.newTodo.date = '';
+    };
+
+    $scope.removeTodo = (todos)=>{
+        let removedTodo = $scope.todoList.indexOf(todos)
+        $scope.todoList.splice(removedTodo, 1);
+      };
+}]);
